952e654047345277a260ce52960d79302d6faa80
Diana <diana.norva@yandex.ru>
2022-10-21 21:46:40 +0300

    Lesson8

---
+++ python/Homework5/Task1.py
@@ -0,0 +1,23 @@
+""" 
+Напишите программу, удаляющую из текста все слова, 
+содержащие ""абв"". """
+
+# text = 'Вы долабвжны выуабвчить правилаабв иабвгры. А затабвем вы доабвлжны играабвть лучшабве, чеабвабвм кто-лиабвбо дабвругой.'
+
+# def find_text(text):
+#     text = list(filter(lambda x: 'абв' not in x, text.split()))
+#     return ' '.join(text)
+
+# text = find_text(text)
+# print(text)
+
+# txt = input("Введите текст через пробел:\n")
+# print(f"Исходный текст: {txt}")
+# find_txt = "абв"
+# lst = [i for i in txt.split() if find_txt not in i]
+# print(f'Результат: {" ".join(lst)}')
+
+
+a = 5//2
+b = 5%2
+print(a>b)
